aa8daa43f5f28172e552ff62d70be98be0ec8d34
Ashok <jillaashok19121999@gmail.com>
2022-05-01 02:09:54 +0530

    Project Complete

--- src/Screen/NowPlaying.js
+++ src/Screen/NowPlaying.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Container from '../components/Container';
 import Movie from '../components/Movie';
 
-const NowPlaying = () => {
+const NowPlaying = ({ navigation }) => {
     const [text, settext] = useState('');
     const parentCallback = (value) => {
         settext(value)
@@ -12,6 +12,7 @@ const NowPlaying = () => {
             <Movie
                 Url={'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed'}
                 filterdata={text}
+                navigation={navigation}
             />
         </Container>
     )
--- src/Screen/TopRated.js
+++ src/Screen/TopRated.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Container from '../components/Container';
 import Movie from '../components/Movie';
 
-const TopRated = () => {
+const TopRated = ({ navigation }) => {
     const [text, settext] = useState('');
     const parentCallback = (value) => {
         settext(value)
@@ -12,6 +12,7 @@ const TopRated = () => {
             <Movie
                 Url={'https://api.themoviedb.org/3/movie/top_rated?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed'}
                 filterdata={text}
+                navigation={navigation}
             />
         </Container>
     )
